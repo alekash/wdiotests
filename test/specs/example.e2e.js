2980614ff022f9779d981edaa72979b6451da4b4
Aleksandra Kashirskaya <shurasanyatx@gmail.com>
2021-03-20 00:22:03 -0400

    first test

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,14 +1,14 @@
 import LoginPage from  '../pageobjects/login.page';
-import SecurePage from '../pageobjects/secure.page';
+import ProfilePage from '../pageobjects/profile.page';
 
-describe('My Login application', () => {
-    it('should login with valid credentials', () => {
+describe('Auth', () => {
+    it('user logs in with valid data', () => {
         LoginPage.open();
+         LoginPage.setLogin('pimob39177@naymeo.com');
+         LoginPage.setPassword('Murzilka21');
+         LoginPage.clickSubmitButton();
+         ProfilePage.isOpen();
 
-        LoginPage.login('tomsmith', 'SuperSecretPassword!');
-        expect(SecurePage.flashAlert).toBeExisting();
-        expect(SecurePage.flashAlert).toHaveTextContaining(
-            'You logged into a secure area!');
     });
 });
 
